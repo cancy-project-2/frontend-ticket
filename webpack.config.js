e9d3ee256f4a436276b31f77a447da097c8b163d
Cyrus Mang <cyrus4198@gmail.com>
2022-05-28 15:45:57 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,18 @@ module.exports = (webpackConfigEnv, argv) => {
     projectName: "ticket",
     webpackConfigEnv,
     argv,
+    standaloneOptions: {
+      appOrParcelName: "ticket",
+      importMapUrl: new URL("https://storage.googleapis.com/cancy-website-bucket/import/map.json"),
+    },
   });
 
   return merge(defaultConfig, {
+    externals: [
+      "react",
+      "react-dom",
+      "single-spa", /^@cancy-project\//
+    ],
     // modify the webpack config however you'd like to by adding to this object
   });
 };
